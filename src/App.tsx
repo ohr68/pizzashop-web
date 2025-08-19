70b88f6311f9392f0e635dd3bc2c4ca7d023bf26
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2025-08-19 20:00:14 -0300

    feat: :sparkles: Adiciona api client e query client

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { HelmetProvider, Helmet } from '@dr.pogodin/react-helmet'
 import './styles/styles.css'
 import { router } from './routes'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
 
 export function App () {
   return (
@@ -12,7 +14,9 @@ export function App () {
       <Helmet titleTemplate='%s | pizza.shop' />
       <ThemeProvider storageKey='pizzashop-theme' defaultTheme='dark'>
         <Toaster richColors theme='system' />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
