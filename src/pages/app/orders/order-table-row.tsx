c682b65db9dd418a81efb714716eda22c7716fd5
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2025-08-20 12:04:46 -0300

    feat: :sparkles: Adiciona paginação de pedidos

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,10 +1,24 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 import { Button } from '@/components/ui/button'
 import { TableCell, TableRow } from '@/components/ui/table'
-import { ArrowRight, Search, X } from 'lucide-react'
 import { OrderDetails } from './order-details'
 import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { OrderStatus } from '@/components/order-status'
 
-export function OrderTableRow () {
+interface OrderTableRowProps {
+  order: {
+    orderId: string
+    createdAt: string
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    customerName: string
+    total: number
+  }
+}
+
+export function OrderTableRow ({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -18,18 +32,25 @@ export function OrderTableRow () {
           <OrderDetails />
         </Dialog>
       </TableCell>
-      <TableCell className='font-mono text-xs font-medium'>3ee4ae47-1929-4969-ae93-dbc25d25fd30</TableCell>
-      <TableCell className='text-muted-foreground'>há 15 minutos</TableCell>
-      <TableCell>
-        <div className='flex items-center gap-2'>
-          <span className='h-2 w-2 rounded-full bg-slate-100' />
-          <span className='font-medium text-muted-foreground'>
-            Pendente
-          </span>
-        </div>
+      <TableCell className='font-mono text-xs font-medium'>
+        {order.orderId}
       </TableCell>
-      <TableCell className='font-medium'>Teste</TableCell>
-      <TableCell className='font-medium'>R$ 149,90</TableCell>
+      <TableCell className='text-muted-foreground'>
+        {formatDistanceToNow(new Date(order.createdAt), {
+          locale: ptBR,
+          addSuffix: true
+        })}
+      </TableCell>
+      <TableCell>
+        <OrderStatus status={order.status} />
+      </TableCell>
+      <TableCell className='font-medium'>{order.customerName}</TableCell>
+      <TableCell className='font-medium'>
+        {order.total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        })}
+      </TableCell>
       <TableCell>
         <Button variant='outline' size='xs'>
           <ArrowRight className='mr-2 h-3 w-3' />
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,10 +1,34 @@
+import { useSearchParams } from 'react-router-dom'
+import { z } from 'zod'
+
 import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { OrderTableRow } from './order-table-row'
 import { OrderTableFilters } from './order-table-filters'
 import { Pagination } from '@/components/pagination'
 import { Seo } from '@/components/seo'
+import { useQuery } from '@tanstack/react-query'
+import { getOrders } from '@/api/get-orders'
 
 export function Orders () {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const pageIndex = z.coerce.number()
+    .transform(page => page - 1)
+    .parse(searchParams.get('page') ?? '1')
+
+  const { data: result } = useQuery({
+    queryKey: ['orders', pageIndex],
+    queryFn: () => getOrders({ pageIndex })
+  })
+
+  function handlePaginate (pageIndex: number) {
+    setSearchParams(state => {
+      state.set('page', (pageIndex + 1).toString())
+
+      return state
+    })
+  }
+
   return (
     <>
       <Seo
@@ -33,15 +57,22 @@ export function Orders () {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
+                {result && result.orders.map((order) => {
                   return (
-                    <OrderTableRow key={i} />
+                    <OrderTableRow key={order.orderId} order={order} />
                   )
                 })}
               </TableBody>
             </Table>
           </div>
-          <Pagination pageIndex={0} totalCount={105} perPage={10} />
+          {result && (
+            <Pagination
+              onPageChange={handlePaginate}
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+            />
+          )}
         </div>
       </div>
     </>
