feb64fc870e91402e3463c36ce4a924dca93afa0
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2025-08-19 21:01:57 -0300

    feat: :sparkles: Adiciona feature de obtenção de dados do perfil do usuário e do restaurante

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -9,8 +9,28 @@ import {
   DropdownMenuTrigger
 } from './ui/dropdown-menu'
 import { Button } from './ui/button'
+import { useQuery } from '@tanstack/react-query'
+import { getProfile } from '@/api/get-profile'
+import { getManagedRestaurant } from '@/api/get-manage-restaurant'
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu () {
+  const {
+    data: profile,
+    isLoading: isLoadingProfile
+  } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile
+  })
+
+  const {
+    data: managedRestaurant,
+    isLoading: isLoadingManagedRestaurant
+  } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant
+  })
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -18,16 +38,31 @@ export function AccountMenu () {
           variant='outline'
           className='flex items-center gap-2'
         >
-          Pizza Shop
+          {isLoadingManagedRestaurant
+            ? (
+              <Skeleton className='h-4 w-40' />
+              )
+            : managedRestaurant?.name}
           <ChevronDown className='h-4 m-4' />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end' className='w-56'>
         <DropdownMenuLabel className='flex flex-col'>
-          <span>Otávio Odenik</span>
-          <span className='text-xs font-normal text-muted-foreground'>
-            otavio.odenik@gmail.com
-          </span>
+          {isLoadingProfile
+            ? (
+              <div className='space-y-1.5'>
+                <Skeleton className='h-4 w-32' />
+                <Skeleton className='h-3 w-24' />
+              </div>
+              )
+            : (
+              <>
+                <span>{profile?.name}</span>
+                <span className='text-xs font-normal text-muted-foreground'>
+                  {profile?.email}
+                </span>
+              </>
+              )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
